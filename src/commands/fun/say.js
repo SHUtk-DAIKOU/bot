d61d61cdcc4f2e99372fc14bd79cae783bd347e8
Koro soft <104771756+Otatoge@users.noreply.github.com>
2022-07-23 21:00:38 +0900

    形式変更に伴う変更

--- src/commands/fun/say.js
+++ src/commands/fun/say.js
@@ -1,14 +1,14 @@
 module.exports = {
     exec: async (command, client) => {
+        const tmpReply = require('../lib/tmpReply');
         const args = require('../lib/args')(command.content);
         if (args.join(' ').length <= 2000) {
             return command.channel.send(args.join(' '));
         } else {
-            const errMsg = await command.reply('文字制限を超える内容のため、実行できません');
-            setTimeout(() => errMsg.delete(), 5000);
+            tmpReply(command, '文字制限を超える内容のため、実行できません');
         }
-    }, 
-    help: '使い方: `say ...内容`\n内容をボットに発言させます', 
+    },
+    help: '使い方: `say ...内容`\n内容をボットに発言させます',
     minArgs: 2,
     maxArgs: Number.MAX_SAFE_INTEGER
 }
