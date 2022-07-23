9e0f2679a48c60e159a8d5df837bd6c29c3f386a
Otatoge <104771756+Otatoge@users.noreply.github.com>
2022-07-23 16:57:23 +0900

    コマンドハンドラを新しくする&字数制限に対応

--- src/commands/fun/say.js
+++ src/commands/fun/say.js
@@ -1,4 +1,14 @@
-module.exports = (command, client) => {
-    const args = require('../lib/args')(command.content);
-    return command.channel.send(args.join(' '));
+module.exports = {
+    exec: async (command, client) => {
+        const args = require('../lib/args')(command.content);
+        if (args.join(' ').length <= 2000) {
+            return command.channel.send(args.join(' '));
+        } else {
+            const errMsg = await command.reply('文字制限を超える内容のため、実行できません');
+            setTimeout(() => errMsg.delete(), 5000);
+        }
+    }, 
+    help: '使い方: `say ...内容`\n内容をボットに発言させます', 
+    minArgs: 2
+    maxArgs: Number.MAX_SAFE_INTEGER
 }
