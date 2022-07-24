fe02c9eca7052a7657fb6d89064a0feac0329097
Koro soft <104771756+Otatoge@users.noreply.github.com>
2022-07-25 07:46:40 +0900

    envの形式更新とエラー対応

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, MessageEmbed } = require('discord.js');
 const client = new Client({
     intents: Object.keys(Intents.FLAGS)
 });
 
-const prefix = 'o!'
-const commandNotFound = ":x: コマンドが見つかりません"
+const cnf = JSON.parse(process.env.CONFIG);
+
 const commands = require('./src/commands');
 const listeners = require('./src/event');
 
@@ -17,9 +17,9 @@ listeners.forEach((listener) => {
 });
 
 client.on('messageCreate', (message) => {
-    if (message.content.startsWith(prefix) && message.author !== client.user) {
+    if (message.content.startsWith(cnf.prefix) && message.author !== client.user) {
         const args = message.content.split(' ');
-        const cmd = args[0].replace(prefix, '');
+        const cmd = args[0].replace(cnf.prefix, '');
         if (commands.list.includes(cmd)) {
             const cmdres = require(commands.cmdPos[commands.list.indexOf(cmd)]);
             if (args.length >= cmdres.minArgs && args.length <= cmdres.maxArgs) {
@@ -28,9 +28,36 @@ client.on('messageCreate', (message) => {
                 tmpReply(message, cmdres.help, 3000);
             }
         } else {
-            tmpReply(message, commandNotFound);
+            tmpReply(message, cnf.notCmd);
         }
     }
+});
+
+client.on('error', async (err) => {
+    (await client.channels.fetch(cnf.logCh)).send({embeds: [new MessageEmbed({
+        title: 'エラーが発生しました',
+        color: 'RED',
+        description: err,
+        timestamp: new Date()
+    })]});
+});
+
+process.on('uncaughtException', async (err) => {
+    (await client.channels.fetch(cnf.logCh)).send({embeds: [new MessageEmbed({
+        title: 'エラーが発生しました',
+        color: 'RED',
+        description: err,
+        timestamp: new Date()
+    })]});
+});
+
+process.on('unhandledRejection', async (reason) => {
+    (await client.channels.fetch(cnf.logCh)).send({embeds: [new MessageEmbed({
+        title: 'Promiseでエラーが発生しました',
+        color: 'ORANGE',
+        description: reason,
+        timestamp: new Date()
+    })]});
 });
 
 client.login().then(() => console.log('Logged in!')).catch(console.error);
